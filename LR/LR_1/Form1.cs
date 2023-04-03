54efd478b3cab8287da2edf1f0eeeaefb7938616
dll31 <danila.litvinov31@yandex.ru>
2023-04-03 14:26:43 +0300

    typo in dll name in c#

--- LR/LR_1/Form1.cs
+++ LR/LR_1/Form1.cs
@@ -101,7 +101,7 @@ namespace LR_1
 
     public static class MfcDll
     {
-        [DllImport(@"MemMappedFileFinctions.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+        [DllImport(@"MemMappedFileFunctions.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
         public static extern void mapsend(int addr, string str);
     }
 
